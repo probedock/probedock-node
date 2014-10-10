37f8fc4ba2d4db276b9831e47ff7f7619af84ff5
Simon Oulevay <simon.oulevay@lotaris.com>
2014-10-10 16:59:20 +0200

    Extracted rox api.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,23 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+
     jshint: {
       all: [
         'Gruntfile.js',
         'lib/**/*.js'
       ]
+    },
+
+    jasmine_node: {
+      all: ['spec/']
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-jasmine-node');
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'jasmine_node']);
 };
---
+++ spec/config.spec.js
@@ -0,0 +1,4 @@
+describe("config", function() {
+
+  
+});
