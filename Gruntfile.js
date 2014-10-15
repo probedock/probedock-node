1e466e152c664c65707f5ee1ed7daa5e40da04da
Simon Oulevay <simon.oulevay@lotaris.com>
2014-10-15 14:37:12 +0200

    Version bump to 0.1.1.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,6 +16,7 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-bump');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-jasmine-node');
 
